bb5a7ff21319d482f859996983773d77fd67ccd0
unknown <vantanhung2000@gmail.com.vn>
2018-02-15 16:14:23 +0700

    lesson05-handle-events

---
+++ src/components/Product.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // this.addToCard = this.addToCard.bind(this);
+  }
+
+  addToCard = () => {
+    console.log('Da mua thanh cong' + this.props.name);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <div className="thumbnail">
+            <img width="180" height="180" alt={this.props.name} src={this.props.image}/>
+            <div className="caption">
+              <h4>{this.props.name}</h4>
+              <p>
+                {this.props.price} VNĐ
+              </p>
+              <p>
+                <a className="btn btn-primary" onClick={ this.addToCard }>Mua ngay</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Product;
